8572d3126737c82cea3ad024c314271dc5cf2185
Shubham Umap <shubhamvumap@gmail.com>
2025-04-15 00:48:28 +0530

    remove unneccessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-// import { Toaster } from "@/components/ui/toaster";
-// import { Toaster as Sonner } from "@/components/ui/sonner";
-// import { TooltipProvider } from "@/components/ui/tooltip";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { TaskProvider } from "./context/TaskContext";
@@ -13,9 +11,6 @@ import NotFound from "./Pages/NotFound";
 const App = () => (
   <AuthProvider>
     <TaskProvider>
-      {/* <TooltipProvider> */}
-        {/* <Toaster /> */}
-        {/* <Sonner /> */}
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
@@ -25,7 +20,6 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
-      {/* </TooltipProvider> */}
     </TaskProvider>
   </AuthProvider>
 );
